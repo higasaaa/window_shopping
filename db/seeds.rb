c92b0983fe355527fa434a1bd11d6b929c23733f
higasaaa <higasaaa26@gmail.com>
2023-03-18 14:10:03 +0000

    readmeの訂正

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,15 +40,15 @@
 # コーディネートの投稿
   Coordinate.create!(
     [
-      {tag_id: Tag.all[0].id, total_price: 8000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate1.jpg"), filename:"sample-coordinate1.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[1].id, total_price: 10000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate2.jpg"), filename:"sample-coordinate2.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[2].id, total_price: 12000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate4.jpg"), filename:"sample-coordinate4.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[3].id, total_price: 7000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate3.jpg"), filename:"sample-coordinate3.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[0].id, total_price: 8000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate5.jpg"), filename:"sample-coordinate5.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[3].id, total_price: 9000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate6.jpg"), filename:"sample-coordinate6.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[3].id, total_price: 13000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate7.jpg"), filename:"sample-coordinate7.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[4].id, total_price: 12000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate8.jpg"), filename:"sample-coordinate8.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'},
-      {tag_id: Tag.all[3].id, total_price: 13000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate9.jpg"), filename:"sample-coordinate9.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるオススメアイテムです。'}
+      {tag_id: Tag.all[0].id, total_price: 8000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate1.jpg"), filename:"sample-coordinate1.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。また、上質な生地の持つ着心地感とスタイルアップをサポートしてくれるおすすめアイテムです。'},
+      {tag_id: Tag.all[1].id, total_price: 10000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate2.jpg"), filename:"sample-coordinate2.jpg"), coordinates_description: 'シースルーのトップスが女らしさをUPさせてくれます。パンツは裾幅が広めなので美脚に見せてくれるおすすめのコーディネートです。'},
+      {tag_id: Tag.all[2].id, total_price: 12000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate4.jpg"), filename:"sample-coordinate4.jpg"), coordinates_description: 'トップスがエレガントさを演出してくれます。ハイウエストパンツと合わせることで足長効効果が期待できるおすすめのコーディネートです。'},
+      {tag_id: Tag.all[3].id, total_price: 7000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate3.jpg"), filename:"sample-coordinate3.jpg"), coordinates_description: '美しいシルエットを描くフィット感ロングワンピースです。ドット柄が可愛らしいですが上品な雰囲気を持つおすすめのアイテムです。'},
+      {tag_id: Tag.all[0].id, total_price: 8000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate5.jpg"), filename:"sample-coordinate5.jpg"), coordinates_description: 'ゆったりシルエットの体型をカバーしてくれるワンピースです。襟元のフリルが可愛らし印象にしてくれるおすすめのコーディネートです。'},
+      {tag_id: Tag.all[3].id, total_price: 9000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate6.jpg"), filename:"sample-coordinate6.jpg"), coordinates_description: '胸元の切り替えのフリルが印象的なブラウス、デニムパンツを合わせることによってカジュアルダウンをしたおすすめのコーディネートです。'},
+      {tag_id: Tag.all[3].id, total_price: 13000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate7.jpg"), filename:"sample-coordinate7.jpg"), coordinates_description: '全身黒で揃えたモード感漂うコーディネートです。シンプルなので色々なアイテムに着回しが効いて使い勝手が◎のおすすめのコーディネートです。'},
+      {tag_id: Tag.all[4].id, total_price: 12000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate8.jpg"), filename:"sample-coordinate8.jpg"), coordinates_description: 'オーバーサイズシルエットのチェック柄のセットアップです。インナーは肌が少し見えるのがポイントです。着回しがきくおすすめのコーディネートです。'},
+      {tag_id: Tag.all[3].id, total_price: 13000, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-coordinate9.jpg"), filename:"sample-coordinate9.jpg"), coordinates_description: 'フリル、袖口がポイントのブラウスです。パンツは、着丈が少し短めなので足元がスッキリした印象になります。'}
     ]
   )
   puts "coordinateを作成しました"
@@ -71,8 +71,7 @@
       )
     end
   end
-  
-  # コメントのデータ
-  
- 
- 
+
+
+
+
